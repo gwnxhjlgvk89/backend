ac2674d00beb8297fba16424ba8a5cdb7e879291
GG1214 <qq1270414897@gmail.com>
2026-03-24 02:57:41 +0800

    test

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=8000,
         reload=True,
         timeout_keep_alive=5,
