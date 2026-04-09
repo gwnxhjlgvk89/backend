aca0c461dda4ce0c9532ae5c2724832276d066ff
GG <qq1270414897@gmail.com>
2026-04-09 10:32:29 +0800

    fix

--- routers/student/student.py
+++ routers/student/student.py
@@ -351,11 +351,11 @@ def get_club_list(
     student: models.Students = Depends(get_current_student),
     db: Session = Depends(get_db),
 ):
-    # 2️⃣ 缓存不存在，查询数据库
-    clubs_data = get_clubs_with_major_restrictions(db)
+    # # 2️⃣ 缓存不存在，查询数据库
+    # clubs_data = get_clubs_with_major_restrictions(db)
 
-    # 3️⃣ 存入Redis
-    ClubListCache.set(clubs_data)
+    # # 3️⃣ 存入Redis
+    # ClubListCache.set(clubs_data)
 
     # 1️⃣ 先从Redis获取缓存
     cached_data = ClubListCache.get()
